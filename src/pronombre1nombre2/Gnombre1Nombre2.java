c678549246f5e829043db83766791c559dd0817c
Labanda34 <juann890@gmail.com>
2022-05-14 13:06:52 +0200

    cambio remoto

--- src/pronombre1nombre2/Gnombre1Nombre2.java
+++ src/pronombre1nombre2/Gnombre1Nombre2.java
@@ -1,13 +1,17 @@
 package pronombre1nombre2;
-
+import java.util.Scanner;;
 public class Gnombre1Nombre2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int edad;
 
+edad=33;
+		System.out.println("mi nombre es juan ");
 		
-		System.out.println("hola mi nombre es juan");
+		System.out.println("mi segundo nombre es ignacio");
 		
+		System.out.println("y tengo "+edad+" años");
 	}
 
 }
