eeba0f206159128eb10de8b4a7fc36b9c16ed3b3
Labanda34 <juann890@gmail.com>
2022-05-15 13:16:20 +0200

    constructor nuevo

---
+++ src/nueevopaq/Casa.java
@@ -0,0 +1,40 @@
+package nueevopaq;
+import java.util.Scanner;
+public class Casa {
+Scanner teclado=new Scanner (System.in);
+String calle;
+int num,cp;
+void Direccion () {
+System.out.println("teclea nombre la calle");
+calle=teclado.next();
+System.out.println("teclea en numero de la calle");
+num=teclado.nextInt();
+System.out.println("teclea el codigo postal");
+cp=teclado.nextInt();
+}
+
+void Busqueda() {
+if(cp==48610) {
+System.out.println("usted pertenece a uribe costa");	
+	
+}else {
+	
+System.out.println("usted pertenece al gran bilbao");	
+}
+	
+	
+}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Casa obj=new Casa();
+obj.Direccion();
+obj.Busqueda();
+		
+		
+
+	}
+
+}
